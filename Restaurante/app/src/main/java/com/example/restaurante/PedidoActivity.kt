0172c127e417785b0210be8057476788271c12a2
AlvaroARVerissimo <166779166+AlvaroARVerissimo@users.noreply.github.com>
2024-05-09 17:59:52 -0300

    Update PedidoActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
@@ -5,23 +5,36 @@ import android.os.Bundle
 import com.example.restaurante.databinding.ActivityPedidoBinding
 
 class PedidoActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPedidoBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityPedidoBinding.inflate(layoutInflater)
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+   // Declaração de uma variável 'binding' que será inicializada posteriormente.
+// Esta variável será usada para acessar e manipular os elementos da interface do usuário.
+private lateinit var binding: ActivityPedidoBinding
 
-        val i = intent
-        val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
-        val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
-        val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
+// Método onCreate é chamado quando a atividade é criada.
+override fun onCreate(savedInstanceState: Bundle?) {
+    // Infla o layout da atividade de pedido e inicializa a variável 'binding'.
+    binding = ActivityPedidoBinding.inflate(layoutInflater)
+    
+    // Chama o método onCreate da superclasse.
+    super.onCreate(savedInstanceState)
+    
+    // Define o layout da atividade como a visualização raiz no arquivo de layout vinculado.
+    setContentView(binding.root)
 
-        val texto = "Resumo do Pedido\n" +
+    // Recupera a intenção que iniciou esta atividade.
+    val i = intent
+    
+    // Recupera as quantidades de pizza, salada e hambúrguer passadas como extras na intenção.
+    val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
+    val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
+    val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
+
+    // Cria um texto de resumo do pedido, incluindo a quantidade e o preço de cada item.
+    val texto = "Resumo do Pedido\n" +
                 "Pizza: $quantidadePizza Preço: ${quantidadePizza*8}\n" +
                 "Salada: $quantidadeSalada Preço: ${quantidadeSalada*10}\n" +
                 "Hamburguer: $quantidadeHamburguer Preço: ${quantidadeHamburguer*12}\n"
 
-        binding.textResumo.text = texto
-
-    }
+    // Define o texto do elemento 'textResumo' como o texto de resumo do pedido.
+    binding.textResumo.text = texto
+}
 }
